b644bf2ca589550bcd44c591f58e5fb19a085c77
Amit Desai <amit.desai03@gmail.com>
2017-09-13 17:18:37 -0700

    Update lc-monkey.js

--- lc-monkey.js
+++ lc-monkey.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         LeetCode
+// @name         LC
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Make LeetCode a bit better
@@ -9,51 +9,74 @@
 /* jshint -W097 */
 'use strict';
 
-console.log("hello from tampermonkey");
-
-// add question id to the question detail page
-var questionIdNode = document.querySelector('input[name="question_id"]');
-if (questionIdNode) {
-    var questionId = questionIdNode.value;
-    var node = document.createElement('span');
-    node.appendChild(document.createTextNode('#' + questionId));
-    var titleNode = document.querySelector('.question-title');
-    titleNode.appendChild(node);
-
-    // styling
-    node.style.color = "#BBB";
-    node.className = 'question-id';
-    node.style.marginLeft = '5px';
-}
-
-function hideElement(query) {
-    var node = document.querySelector(query);
-    if (node) {
-        node.style.display = 'none'
+function remove(id,className){
+    if(className!==null && document.getElementsByClassName(className).length>0){
+        document.getElementsByClassName(className)[0].remove();
+    }
+    if(id!==null && document.getElementById(id)!==null){
+        document.getElementById(id).remove();
     }
 }
 
-// hide the ugly subscribtion and book links
-hideElement('#subscribe'); // on the problem page
-hideElement('a[href="/subscribe"]'); // on the discuss page
-hideElement('.qa-sidebar'); // discussion sidebar
-hideElement('#bookNav');
-hideElement('#chat-link'); // Send Feedback
-hideElement('footer');
-hideElement('#survey'); // Have you met this question in a real interview? No
-
-// add statictics in the /company/*/ page
-if (document.location.pathname.startsWith('/company/')) {
-    var stat = document.querySelectorAll('.ac').length + '/' + document.querySelectorAll('tr').length;
-    var node = document.createElement('span');
-    node.textContent = stat;
-    var header = document.querySelector('h3');
-    header.appendChild(node);
-    node.style.fontSize = '14px';
-    node.style.color = '#BBB';
-    
+function toggle(element){
+    if(element.style.display=='none' || element.style.display==''){
+        element.style.display='block';
+    }else if(element.style.display=='block'){
+        element.style.display='none';
+    }
 }
-// hide non-free questions on the problem set page
-//Array.prototype.forEach.call(document.querySelectorAll('.fa-lock'), function (n) {
-//    n.parentElement.parentElement.style.display = "none";
-//});
+function toggleEither(id,className){
+
+    if(className!==null && document.getElementsByClassName(className).length>0){
+        toggle(document.getElementsByClassName(className)[0]);
+    }
+    if(id!==null && document.getElementById(id)!==null){
+        toggle(document.getElementById(id));
+    }
+}
+
+
+function createButtonForToggle(id,buttonName){
+
+    if(document.getElementById(id)!==null){
+            document.getElementById(id).style.display='none';
+    }else if(document.getElementsByClassName(id).length!==0){
+        document.getElementsByClassName(id)[0].style.display='none';
+    }
+    else return;
+
+
+    // 1. Create the button
+    var button = document.createElement("button");
+    button.innerHTML = buttonName;
+
+    // 2. Append somewhere
+    var body = document.getElementsByTagName("body")[0];
+    body.insertBefore(button, body.firstChild);
+
+
+    // 3. Add event handler
+    button.addEventListener ("click", function() {
+        toggleEither(null,id);
+    });
+}
+remove(null,'navbar');
+remove(null,'question-title');
+remove(null,'side-bar');
+remove(null,'action-btn-base');
+remove('interviewed-div',null);
+remove(null,'site-footer');
+remove(null,'notepad-wrapper');
+remove(null,'tab-view');
+remove(null,'theme-select');
+remove(null,'reset-btn');
+remove(null,'contribute-btn');
+
+document.getElementsByName("lang")[0].selectedIndex=1;
+document.getElementsByName("lang")[0].style.display = 'none';
+
+createButtonForToggle('action','3');
+createButtonForToggle('control-btn-bar','2');
+createButtonForToggle('question-detail-container','1');
+
+document.title = "Google";
